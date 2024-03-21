c6ed8d244ef0c3da5fd60ba91a8579558c196a35
IsoDevMate <oumaoduor5827@gmail.com>
2024-03-21 07:42:13 +0300

    checkout error

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,8 +6,6 @@ import Error from './Error';
 import { SignUpForm } from './auth/signup';
 import { SignIn } from './auth/signin';
 import { Home } from './components/Home';
-import { Checkout } from './components/checkout/checkout';
-
 import { Services } from './components/serviceoffered/layout';
 import { ProfilePage } from './components/profile/profile';
 import { Queue } from './components/queue';
@@ -43,7 +41,6 @@ function App() {
           <Route path="/signin" element={<SignIn />} />
           <Route path="/about" element={<About />} /> 
           <Route path="/contact" element={<Contact />} />
-          <Route path="/checkout" element={<Checkout />} />
           <Route path="/services" element={<Services />} />
           <Route path="/profile" element={<ProfilePage user={user} />} />
           <Route path="/resetpassword" element={<ResetPassword />} />
