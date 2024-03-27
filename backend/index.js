d1e2e7808bbd43f18a6fbe5b52984e5847ef92b5
IsoDevMate <oumaoduor5827@gmail.com>
2024-03-27 17:19:07 +0300

    ...

--- backend/index.js
+++ backend/index.js
@@ -13,6 +13,8 @@ const sendgridMail = require('@sendgrid/mail');
 const multer = require('multer');
 const path = require('path');
 const firebase = require('firebase-admin');
+const axios = require('axios');
+const { HTTPException } = require('hono/http-exception')
 //const { nanoid } = require('nanoid/non-secure');
 const AfricasTalking = require('africastalking')
 
@@ -77,39 +79,6 @@ const hashPassword = async (password) => {
 app.get('/', (req, res) => {
   res.send('Welcome to the Cyber Cafe API');
 })
-
-
-
-
-
-
-
-app.post('/stri-webhook', express.json({type: 'application/json'}), (request, response) => {
-  const event = request.body;
-
-  // Handle the event
-  switch (event.type) {
-    case 'payment_intent.succeeded':
-      const paymentIntent = event.data.object;
-      // Then define and call a method to handle the successful payment intent.
-      // handlePaymentIntentSucceeded(paymentIntent);
-      console.log('Payment intent succeeded:', paymentIntent);
-      break;
-    case 'payment_method.attached':
-      const paymentMethod = event.data.object;
-      // Then define and call a method to handle the successful attachment of a PaymentMethod.
-      // handlePaymentMethodAttached(paymentMethod);
-      console.log('Payment method attached:', paymentMethod);
-      break;
-    // ... handle other event types
-    default:
-      console.log(`Unhandled event type ${event.type}`);
-  }
-
-  // Return a response to acknowledge receipt of the event
-  response.json({received: true});
-});
-
 
 // webhook route
 app.post(
